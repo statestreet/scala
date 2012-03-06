b0383acc6484533c6e8e3c36294cd02088656a1c
Yixiu <yixiugg@126.com>
2012-03-06 12:44:07 +0800

    add readme

---
+++ HelloScala/src/org/yixiugg/hello/TimerAnonymous.scala
@@ -0,0 +1,14 @@
+package org.yixiugg.hello
+
+
+object TimerAnonymous{ 
+  
+      def oncePerSecond(callback: () => Unit) { 
+              while (true) { callback(); Thread sleep 1000 } 
+      } 
+      def main(args: Array[String]) { 
+              oncePerSecond(() => 
+                      println("time flies like an arrow...")) 
+      } 
+} 
+
